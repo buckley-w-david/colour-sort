73a6351b57d0e6b43ca0968b9f09d667aa76ae15
David Buckley <buckley.w.david@gmail.com>
2018-04-11 19:38:50 -0400

    throw away alpha channel of png

--- tests/test_image.py
+++ tests/test_image.py
@@ -9,7 +9,7 @@ def test_as_sorted() -> None:
     directory, _ = os.path.split(__file__)
     test_file_path = os.path.join(directory, 'test.png')
     with open(test_file_path, 'rb') as file:
-        input_image = Image.open(file)
+        input_image = Image.open(file).convert('RGB')
         output = image.as_sorted(input_image)
 
     pic = np.reshape(np.array(output), (IMAGE_SIZE*IMAGE_SIZE, 3))
