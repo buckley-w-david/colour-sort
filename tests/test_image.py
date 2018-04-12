4838edcfa9068cc2ac8917289804a6970876d98d
David Buckley <buckley.w.david@gmail.com>
2018-04-12 12:27:56 -0400

    make the RGB tests separate instead of all in 1 test

--- tests/test_image.py
+++ tests/test_image.py
@@ -2,6 +2,7 @@ import itertools
 import numpy as np
 from PIL import Image
 import pytest
+import _pytest
 from colour_sort.image import as_sorted
 
 
@@ -21,22 +22,23 @@ def test_as_sorted_brightness(image: Image.Image) -> None:
     assert unique.shape == structured_pic.shape
 
 
-def test_as_sorted_rgb(image: Image.Image) -> None:
-    modes = list(
-        ''.join(tup)
-        for tup in itertools.permutations('rgb', 3)
-    )
-    for mode in modes:
-        output = as_sorted(image, mode)
+@pytest.fixture(params=list(''.join(tup)for tup in itertools.permutations('rgb', 3)))
+def rand_image(image, request: _pytest.fixtures.SubRequest) -> Image.Image:
+    return (image, request.param)
 
-        pic = np.reshape(np.array(output), (IMAGE_SIZE*IMAGE_SIZE, 3))
-        structured_pic = np.core.records.fromarrays(pic.transpose(),
-                                                    names='r, g, b',
-                                                    formats='u1, u1, u1')
 
-        unique = np.unique(structured_pic)
+def test_as_sorted_rgb(rand_image: typing.Tuple[Image.Image, str]) -> None:
+    image, mode = rand_image
+    output = as_sorted(image, mode)
 
-        assert unique.shape == structured_pic.shape
+    pic = np.reshape(np.array(output), (IMAGE_SIZE*IMAGE_SIZE, 3))
+    structured_pic = np.core.records.fromarrays(pic.transpose(),
+                                                names='r, g, b',
+                                                formats='u1, u1, u1')
+
+    unique = np.unique(structured_pic)
+
+    assert unique.shape == structured_pic.shape
 
 
 def test_as_sorted_unknown(image: Image.Image) -> None:
