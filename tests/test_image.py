d6a1df721903382b0517f2d8c1b18841fec15e56
David Buckley <buckley.w.david@gmail.com>
2019-11-01 15:16:49 -0400

    refactor tests

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,47 +1,23 @@
-import itertools
-import typing
 import numpy as np
 from PIL import Image
 import pytest
-import _pytest
-from colour_sort.image import as_sorted
+from colour_sort.image import create_sorted_image, SortType
+from colour_sort.verify import verify_image
 
 
-IMAGE_SIZE = 4096
+@pytest.fixture(scope='session')
+def image():
+    data = np.random.randint(0, 255, size=(4096, 4096, 3), dtype='u1')
+
+    return Image.fromarray(data, mode='RGB')
 
 
-def test_as_sorted_brightness(image: Image.Image) -> None:
-    output = as_sorted(image, 'brightness')
-
-    pic = np.reshape(np.array(output), (IMAGE_SIZE*IMAGE_SIZE, 3))
-    structured_pic = np.core.records.fromarrays(pic.transpose(),
-                                                names='r, g, b',
-                                                formats='u1, u1, u1')
-
-    unique = np.unique(structured_pic)
-
-    assert unique.shape == structured_pic.shape
-
-
-@pytest.fixture(params=list(''.join(tup)for tup in itertools.permutations('rgb', 3)))
-def rand_image(image, request: _pytest.fixtures.SubRequest) -> Image.Image:
+@pytest.fixture(params=[sort_type for sort_type in SortType])
+def rand_image(image, request):
     return (image, request.param)
 
 
-def test_as_sorted_rgb(rand_image: typing.Tuple[Image.Image, str]) -> None:
+def test_as_sorted(rand_image):
     image, mode = rand_image
-    output = as_sorted(image, mode)
-
-    pic = np.reshape(np.array(output), (IMAGE_SIZE*IMAGE_SIZE, 3))
-    structured_pic = np.core.records.fromarrays(pic.transpose(),
-                                                names='r, g, b',
-                                                formats='u1, u1, u1')
-
-    unique = np.unique(structured_pic)
-
-    assert unique.shape == structured_pic.shape
-
-
-def test_as_sorted_unknown(image: Image.Image) -> None:
-    with pytest.raises(ValueError):
-        as_sorted(image, 'unknown')
+    output = create_sorted_image(image, mode)
+    assert verify_image(output)
